94b48ce068e5ae2881ed5d1f9a5ff97d198efe53
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-05-10 12:32:00 -0600

    Feat: resume_editor improved

--- lib/resume/resume_editor/set_resume.dart
+++ lib/resume/resume_editor/set_resume.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_use_of_protected_member
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:xapptor_community/resume/models/resume.dart';
 import 'package:xapptor_community/resume/resume_editor/get_resume_ref.dart';
@@ -6,15 +8,18 @@ import 'package:xapptor_community/resume/resume_editor/show_result_snack_bar.dar
 
 extension StateExtension on ResumeEditorState {
   set_resume({
-    required int slot_index,
+    required int new_slot_index,
     required Resume resume,
   }) async {
-    resume.slot_index = slot_index;
+    resume.slot_index = new_slot_index;
     DocumentReference resume_doc_ref = get_resume_ref(
-      slot_index: slot_index,
+      slot_index: new_slot_index,
     );
 
     Map resume_json = resume.to_json();
+
+    slot_index = new_slot_index;
+    setState(() {});
 
     await resume_doc_ref
         .set(
@@ -24,7 +29,7 @@ extension StateExtension on ResumeEditorState {
         .then((value) {
       show_result_snack_bar(
         result_snack_bar_type: ResultSnackBarType.saved,
-        slot_index: slot_index,
+        slot_index: new_slot_index,
       );
     });
   }
