2e50391ddabfb96a5fe003aa16f7cf0dbcb9b93f
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-05-10 12:21:56 -0600

    Feat: resume_editor improved

--- lib/resume/resume_editor/delete_resume.dart
+++ lib/resume/resume_editor/delete_resume.dart
@@ -15,6 +15,7 @@ extension StateExtension on ResumeEditorState {
     await resume_doc_ref.delete().then((value) {
       resumes.removeAt(slot_index);
 
+      slot_index = resumes.first.slot_index;
       load_resume(
         slot_index: resumes.first.slot_index,
       );
