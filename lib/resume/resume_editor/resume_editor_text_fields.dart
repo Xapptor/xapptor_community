985c99df6003c1b8aa1911f24bf8a823112e5aaf
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-08-27 13:46:37 -0600

    Feat: adding spacing in resume_editor_text_fields

--- lib/resume/resume_editor/resume_editor_text_fields.dart
+++ lib/resume/resume_editor/resume_editor_text_fields.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:xapptor_community/resume/resume_editor/resume_editor.dart';
 import 'package:xapptor_community/resume/resume_editor/resume_editor_text_field.dart';
 import 'package:xapptor_logic/form_field_validators.dart';
+import 'package:xapptor_ui/values/ui.dart';
 import 'package:xapptor_ui/widgets/text_field/custom_text_field.dart';
 import 'package:xapptor_ui/widgets/text_field/custom_text_field_model.dart';
 
 extension StateExtension on ResumeEditorState {
   resume_editor_text_fields() => Column(
         children: [
+          SizedBox(height: sized_box_space),
           CustomTextField(
             model: CustomTextFieldModel(
               title: text_list.get(source_language_index)[0],
@@ -21,6 +23,7 @@ extension StateExtension on ResumeEditorState {
               ).validate(),
             ),
           ),
+          SizedBox(height: sized_box_space),
           resume_editor_text_field(
             label_text: text_list.get(source_language_index)[1],
             controller: job_title_input_controller,
@@ -29,6 +32,7 @@ extension StateExtension on ResumeEditorState {
               type: FormFieldValidatorsType.name,
             ).validate(),
           ),
+          SizedBox(height: sized_box_space),
           resume_editor_text_field(
             label_text: text_list.get(source_language_index)[2],
             controller: email_input_controller,
@@ -37,6 +41,7 @@ extension StateExtension on ResumeEditorState {
               type: FormFieldValidatorsType.email,
             ).validate(),
           ),
+          SizedBox(height: sized_box_space),
           resume_editor_text_field(
             label_text: text_list.get(source_language_index)[3],
             controller: website_input_controller,
@@ -45,6 +50,7 @@ extension StateExtension on ResumeEditorState {
               type: FormFieldValidatorsType.email,
             ).validate(),
           ),
+          SizedBox(height: sized_box_space),
           resume_editor_text_field(
             label_text: text_list.get(source_language_index)[5],
             controller: profile_input_controller,
