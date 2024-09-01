2e1ca59bb077d20ea922af59ab938bfb5717b4d4
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-08-31 23:59:28 -0600

    Feat: resume_editor_text_fields improved

--- lib/resume/resume_editor/resume_editor_text_fields.dart
+++ lib/resume/resume_editor/resume_editor_text_fields.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:xapptor_community/resume/resume_editor/resume_editor.dart';
-import 'package:xapptor_community/resume/resume_editor/resume_editor_text_field.dart';
 import 'package:xapptor_logic/form_field_validators.dart';
 import 'package:xapptor_ui/values/ui.dart';
 import 'package:xapptor_ui/widgets/text_field/custom_text_field.dart';
@@ -24,42 +23,62 @@ extension StateExtension on ResumeEditorState {
             ),
           ),
           SizedBox(height: sized_box_space),
-          resume_editor_text_field(
-            label_text: text_list.get(source_language_index)[1],
-            controller: job_title_input_controller,
-            validator: (value) => FormFieldValidators(
-              value: value!,
-              type: FormFieldValidatorsType.name,
-            ).validate(),
+          CustomTextField(
+            model: CustomTextFieldModel(
+              title: text_list.get(source_language_index)[1],
+              hint: text_list.get(source_language_index)[1],
+              focus_node: focus_node_2,
+              on_field_submitted: (fieldValue) => focus_node_3.requestFocus(),
+              controller: job_title_input_controller,
+              validator: (value) => FormFieldValidators(
+                value: value!,
+                type: FormFieldValidatorsType.name,
+              ).validate(),
+            ),
           ),
           SizedBox(height: sized_box_space),
-          resume_editor_text_field(
-            label_text: text_list.get(source_language_index)[2],
-            controller: email_input_controller,
-            validator: (value) => FormFieldValidators(
-              value: value!,
-              type: FormFieldValidatorsType.email,
-            ).validate(),
+          CustomTextField(
+            model: CustomTextFieldModel(
+              title: text_list.get(source_language_index)[2],
+              hint: text_list.get(source_language_index)[2],
+              focus_node: focus_node_3,
+              on_field_submitted: (fieldValue) => focus_node_4.requestFocus(),
+              controller: email_input_controller,
+              validator: (value) => FormFieldValidators(
+                value: value!,
+                type: FormFieldValidatorsType.email,
+              ).validate(),
+            ),
           ),
           SizedBox(height: sized_box_space),
-          resume_editor_text_field(
-            label_text: text_list.get(source_language_index)[3],
-            controller: website_input_controller,
-            validator: (value) => FormFieldValidators(
-              value: value!,
-              type: FormFieldValidatorsType.email,
-            ).validate(),
+          CustomTextField(
+            model: CustomTextFieldModel(
+              title: text_list.get(source_language_index)[3],
+              hint: text_list.get(source_language_index)[3],
+              focus_node: focus_node_4,
+              on_field_submitted: (fieldValue) => focus_node_5.requestFocus(),
+              controller: website_input_controller,
+              validator: (value) => FormFieldValidators(
+                value: value!,
+                type: FormFieldValidatorsType.website,
+              ).validate(),
+            ),
           ),
           SizedBox(height: sized_box_space),
-          resume_editor_text_field(
-            label_text: text_list.get(source_language_index)[5],
-            controller: profile_input_controller,
-            validator: (value) => FormFieldValidators(
-              value: value!,
-              type: FormFieldValidatorsType.email,
-            ).validate(),
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
+          CustomTextField(
+            model: CustomTextFieldModel(
+              title: text_list.get(source_language_index)[5],
+              hint: text_list.get(source_language_index)[5],
+              focus_node: focus_node_5,
+              on_field_submitted: (fieldValue) => null,
+              controller: profile_input_controller,
+              validator: (value) => FormFieldValidators(
+                value: value!,
+                type: FormFieldValidatorsType.multiline_long,
+              ).validate(),
+              keyboard_type: TextInputType.multiline,
+              max_lines: null,
+            ),
           ),
         ],
       );
